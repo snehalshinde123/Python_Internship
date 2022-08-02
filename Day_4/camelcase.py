418a784f59d487bff81fe353712081c9763847d8
snehalshinde123 <shindesnehal772@gmail.com>
2022-08-02 14:12:52 +0530

    notes_3

---
+++ Day_4/camelcase.py
@@ -0,0 +1,37 @@
+import camelcase
+
+c = camelcase.CamelCase()
+
+txt = "shiv is handsome boy, he is very smart"
+
+print(c.hump(txt))
+
+#This method capitalizes the first letter of each word.
+
+#vairables
+x = 5
+y = "snehal"
+print(x)
+print(y)
+
+x="shiv"
+print(x)
+#using type vasting
+x = str(10000000)    # x will be '10000000'
+y = int('500')    # y will be 500
+z = float(50000000000000000000000)
+
+print(x)
+print(y)
+print(z)
+# checking the datatype of vaorables
+print(type(x))
+print(type(y))
+print(type(z))
+
+X= "This is capital x"
+
+print("small x = ",x)
+
+#checking the case sensitivity
+print("capital x = ",X)
---
+++ Day_4/modules.py
@@ -0,0 +1,3 @@
+import os
+import tensorflow
+print("using modules")
